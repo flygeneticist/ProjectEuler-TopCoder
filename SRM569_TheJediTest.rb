7d53b085d7559de35960516a5635d35fa8c6f4d0
flygeneticist <flygeneticist@gmail.com>
2013-08-25 07:18:19 -0400

    Code now tests for optimal solution instead of default assigning Y to largest floor. Fixed array deletion issue.

--- SRM569_TheJediTest.rb
+++ SRM569_TheJediTest.rb
@@ -4,32 +4,35 @@
 
 # Given 1-50 floors with 0-1000 students per floor, find number of Jedi need to cover all students.
 def countSupervisors (students, y, j)
-  # sets up counters to keep track of Jedi need on a floor and overall
-  sorted_students = students.sort
-  levels = students.size
-  total_count = 0
-  floor_count = 0
-
-  # find the floor with highest number of students
-  largest_group = sorted_students.max
-
-  # set Yoda to cover the floor with most students and check if more Jedis are needed
-  while (largest_group - y - (floor_count*j)) > 0
-    floor_count += 1
-  end
-  total_count += floor_count
-  sorted_students.pop
-
-  # check all other floors, if any, and assign only Jedi when not on the most occupied floor.
-  sorted_students.each do |floor|
+  best_count = Hash.new # hash to store various possible results
+  students.each do |i|
+    # sets up counters to keep track of Jedi needed on a floor and overall
     floor_count = 0
-    while (floor - (floor_count*j)) > 0
+    total_count = 0
+    temp_students = students.map {|i| i}
+
+    while (i - y - (floor_count*j)) > 0
       floor_count += 1
     end
     total_count += floor_count
-  end
+    temp_students.delete_at(temp_students.index(i))
 
-  return total_count
+    # check all other floors, if any, and assign only Jedi when not on the most occupied floor.
+    temp_students.each do |floor|
+      floor_count = 0
+      while (floor - (floor_count*j)) > 0
+        floor_count += 1
+      end
+      total_count += floor_count
+    end
+    best_count[i] = total_count
+  end
+  return best_count.values.min
 end
 
-puts countSupervisors([10,1000], 101, 100)
+# Test Cases
+puts countSupervisors([10], 100, 2) # => 0
+puts countSupervisors([10, 15],12,5) # => 3
+puts countSupervisors([0, 0, 0, 0, 0],145,21) # => 0
+puts countSupervisors([11, 13, 15], 9, 5) # => 7
+puts countSupervisors([4, 7, 10, 5, 6, 55, 2], 20, 3) # => 26
