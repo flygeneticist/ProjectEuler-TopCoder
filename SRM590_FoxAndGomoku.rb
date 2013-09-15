3e5fcbb82bb1172d18cc7dd503b4f100d5b9cf51
flygeneticist <flygeneticist@gmail.com>
2013-09-15 13:24:22 -0400

    checks board horizontally and vertically.

--- SRM590_FoxAndGomoku.rb
+++ SRM590_FoxAndGomoku.rb
@@ -2,29 +2,161 @@
 # Problem Name: FoxAndGomoku
 # Author's User Name: flygeneticist
 
-#You are given a String[] board that represents a square board. The character board[i][j] represents the cell with coordinates (i,j). Each of those characters is either '.' (representing an empty cell) or 'o' (representing a cell with Jiro's piece). Determine whether there are 5 consecutive cells (horizontally, vertically, or diagonally) that contain Jiro's tokens. Return "found" (quotes for clarity) if there are five such cells anywhere on the board. Otherwise, return "not found".
+#You are given a String[] board that represents a square board.
+#The character board[i][j] represents the cell with coordinates (i,j).
+#Each of those characters is either '.' (representing an empty cell) or 'o' (representing a cell with a piece).
+#Determine whether there are 5 consecutive cells (horizontally, vertically, or diagonally) that contain Jiro's tokens.
+#Return "found" (quotes for clarity) if there are five such cells anywhere on the board. Otherwise, return "not found".
 
 def consecutive (board)
-  consecutive_zeros = []
-  puts board
-  board.each do |row|
-    row = row.scan(/./)
-    print row
-    row.each do |cell|
-      if cell == "o"
-        consecutive_zeros << [board.index(row),row.index(cell)]
-        # check the surrounding cells for zeros
+  puts
+  puts "~~~ NEW PUZZLE ~~~"
+
+  # setup variables needed
+  consecutive_zeros = 0
+  current_row = -1
+
+  # work through board rows to find consecutive cells horizontally
+  # if first cell in a row <> 'o', jump 4 cells forward for next cell to search
+  for row in (0..(board.length-1)) do
+    consecutive_zeros = 0
+    for cell in (0..(board[0].length-1))
+      if board[row][cell] == "o"
+        consecutive_zeros += 1
+        if consecutive_zeros == 5
+          return "found horizontally"
+        end
+      else
+        consecutive_zeros = 0
       end
     end
   end
+
+  # work through board rows to find consecutive cells vertically
+  for cell in (0..(board[0].length-1)) do
+    consecutive_zeros = 0
+    for row in (0..(board.length-1))
+      if board[row][cell] == "o"
+        consecutive_zeros += 1
+        if consecutive_zeros == 5
+          return "found vertically"
+        end
+      else
+        consecutive_zeros = 0
+      end
+    end
+  end
+  puts "not found"
+
+   # work through board rows to find consecutive cells diagonally
+   # consider the length of a row and the num of rows remaining to determine if possible
+
+end
+
+
+def search_params(board, current_row, current_cell)
+  # define cell positions surrounding the current cell
+  investigate_row = current_row + 1
+
+  if (current_cell + 1) >= board[0].length
+    # No more cells to the right to search!
+    r_cell = 0
+    br_cell = 0
+  else
+    # set all right cells
+    r_cell = [current_row, (current_cell + 1)]
+    br_cell = [investigate_row, (current_cell + 1)]
+  end
+
+  if (current_cell - 1) == -1
+    # No more cells to the left to search!
+    bl_cell = 0
+  else
+    # set bottom-left cells
+    bl_cell = [investigate_row, (current_cell - 1)]
+  end
+
+  if investigate_row >= board.length
+    # at the end of the board. No more rows to search!
+    bl_cell = 0
+    b_cell = 0
+    br_cell = 0
+  else
+    b_cell = [investigate_row, current_cell]
+  end
+
+  surrounding_cells = [r_cell, bl_cell, b_cell, br_cell]
+  return surrounding_cells
+end
+
+def search_surroundings (surrounding_cells)
+  # check the surrounding cells for zeros if search_param is valid (ie. not beyond edges of board)
 end
 
 # Test Cases
-consecutive([".....", ".....", ".....", ".....", "....."]) # => not found
-consecutive(["ooooo", "ooooo", "ooooo", "ooooo", "ooooo"]) # => found
-#consecutive(["....o.", "...o..", "..o...", ".o....", "o.....", "......"]) # => found
-#consecutive(["oooo.", ".oooo", "oooo.", ".oooo", "....."]) # => not found
-#consecutive(["oooo.", ".oooo", "oooo.", ".oooo", "....o"]) # => found
-#consecutive(["o....", "o.o..", "o.o.o", "o.o..", "o...."]) # => found
-#consecutive(["..........", "..........", "..oooooo..", "..o.......", "..o.......", "..oooooo..", ".......o..", ".......o..", "..oooooo..", ".........."]) # => found
-#consecutive(["..........", "..........", "..oooo.o..", "..o.......", "..o.......", "..o.oooo..", ".......o..", ".......o..", "..oooo.o..", ".........."]) # => not found
+puts consecutive(
+[".....",
+ ".....",
+ ".....",
+ ".....",
+ "....."]) # => not found
+
+puts consecutive(
+["ooooo",
+ "ooooo",
+ "ooooo",
+ "ooooo",
+ "ooooo"]) # => found (all)
+
+puts consecutive(
+["....o.",
+ "...o..",
+ "..o...",
+ ".o....",
+ "o.....",
+ "......"]) # => found (diagonally)
+
+puts consecutive(
+["oooo.",
+ ".oooo",
+ "oooo.",
+ ".oooo",
+ "....."]) # => not found
+
+puts consecutive(
+["oooo.",
+ ".oooo",
+ "oooo.",
+ ".oooo",
+ "....o"]) # => found (diagonally)
+
+puts consecutive(
+["o....",
+ "o.o..",
+ "o.o.o",
+ "o.o..",
+ "o...."]) # => found (vertically)
+
+puts consecutive(
+["..........",
+ "..........",
+ "..oooooo..",
+ "..o.......",
+ "..o.......",
+ "..oooooo..",
+ ".......o..",
+ ".......o..",
+ "..oooooo..",
+ ".........."]) # => found (horizontally)
+
+puts consecutive(
+["..........",
+ "..........",
+ "..oooo.o..",
+ "..o.......",
+ "..o.......",
+ "..o.oooo..",
+ ".......o..",
+ ".......o..",
+ "..oooo.o..",
+ ".........."]) # => not found
