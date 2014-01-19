2fb4cf1cfa096acba606b68464dff40b8ae0508d
flygeneticist <flygeneticist@gmail.com>
2014-01-19 09:31:08 -0500

    reworked with numpy and Sieve of Eratosthenes

--- euler_prob_10.py
+++ euler_prob_10.py
@@ -2,61 +2,26 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-import math
 
-def find_sum_primes(prime_limit):
-    primes = []
-    for i in range(1, prime_limit):
-        test = is_prime(i)
-        if test:
-            primes.append(i)
+# Sieve of Eratosthenes
+# 1. Make a list of all numbers from 3 to N skipping even numbers.
+# 2. Find the next number p not yet crossed out. This is a prime.
+#       a. If it is greater than N , go to 5.
+# 3. Cross out all multiples of p which are not yet crossed out.
+# 4. Go to 2.
+# 5. The numbers not crossed out are the primes not exceeding N .
 
-    # return the n-2 prime from the final list since the n-1 will be the first prime over the target prime_limit
-    # return the sum of the primes minus the last prime which will be over the prime limit
-    return (primes[-1],sum(primes))
+import numpy as np 
 
 
-# Miller-Rabin primality test
-# taken from Wiki 
-def _try_composite(a, d, n, s):
-    if pow(a, d, n) == 1:
-        return False
-    for i in range(s):
-        if pow(a, 2**i * d, n) == n-1:
-            return False
-    return True # n  is definitely composite
- 
-def is_prime(n, _precision_for_huge_n=16):
-    if n in _known_primes or n in (0, 1):
-        return True
-    if any((n % p) == 0 for p in _known_primes):
-        return False
-    d, s = n - 1, 0
-    while not d % 2:
-        d, s = d >> 1, s + 1
-    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
-    if n < 1373653: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3))
-    if n < 25326001: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
-    if n < 118670087467: 
-        if n == 3215031751: 
-            return False
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
-    if n < 2152302898747: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
-    if n < 3474749660383: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
-    if n < 341550071728321: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
-    # otherwise
-    return not any(_try_composite(a, d, n, s) 
-                   for a in _known_primes[:_precision_for_huge_n])
- 
-_known_primes = [2, 3]
-_known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
+def find_all_primes_up_to(limit):
+    is_prime = np.ones(limit + 1, dtype=np.bool)
+    for n in xrange(2, int(limit**0.5 + 1.5)): 
+        if is_prime[n]:
+            is_prime[n*n::n] = 0
+    return np.nonzero(is_prime)[0][2:]
 
 
 if __name__ == "__main__":
     prime_limit = input("enter prime number limit: ")
-    print(find_sum_primes(prime_limit))
+    print(sum(find_all_primes_up_to(prime_limit)))
