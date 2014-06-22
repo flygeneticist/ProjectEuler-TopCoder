48d83475c99c226a63fc9d1d70ed00b8bfee2ef8
FlyGeneticist <flygeneticist@gmail.com>
2014-06-22 18:37:31 -0400

    simplifying

--- euler_prob_1.js
+++ euler_prob_1.js
@@ -1,15 +1,9 @@
-//If we list all the natural numbers below 10 that are multiples of 3 or 5, 
-//we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-//Find the sum of all the multiples of 3 or 5 below 1000(ie. x).
-function multiples(x) {
-	var sum = 0;
-	for (var num=x; num>2; num--) {
-		if ((num%5 == 0) || (num%3 == 0)) {
-			sum += num;
-		}
+//Find the sum of all the multiples of 3 or 5 below 1000.
+var sum = 0;
+for (var num=999; num>2; num--) {
+	if ((num%5 == 0) || (num%3 == 0)) {
+		sum += num;
 	}
-	return sum;
 }
 
-alert(multiples(999)); // --> 233168
+alert(sum); // 233168
