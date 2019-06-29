ce3ac8878779f4a2b7c2eaacb495054a2c3a6ec0
Kevin Keller <flygeneticist@gmail.com>
2019-06-29 13:52:32 +0700

    add HashMap dispatcher

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+
+use std::collections::HashMap;
 use std::io;
+// import all problems
 mod problems;
+
 
 fn main() {
     println!("Welcome to my Project Euler program.");
 
+    let mut dispatch = HashMap::new();
+    // add problems to dispacter hash table
+    dispatch.insert(1, problems::p1());
+
     loop {
-        println!("Please enter the number of the Euler problem you wish to run:");
+        println!(
+            "Please enter the number of the Euler problem you wish to run:\n{:?}",
+            dispatch.keys()
+        );
+
         let mut choice = String::new();
         io::stdin()
             .read_line(&mut choice)
@@ -17,8 +29,17 @@ fn main() {
                 continue;
             }
         };
+
         println!("You chose Problem #{}.", choice);
-        problems::run(choice);
-        break;
+
+        if dispatch.contains_key(&choice) {
+            println!("~~~ RUNNING PROBLEM #{} NOW ~~~", choice);
+            let solution = dispatch.get(&choice);
+            println!("Solution is: {:?}", solution);
+            break;
+        } else {
+            println!("That problem is not available.");
+            continue;
+        }
     }
 }
--- src/problems.rs
+++ src/problems.rs
@@ -1,28 +1,15 @@
-pub fn run(choice: u32) {
-    println!("Hello from inside the Problems Module's public function!");
-    println!("~~~ RUNNING PROBLEM #{} NOW ~~~", choice);
-    let solution = prob1();
-    println!("Solution is: {}", solution);
-}
-
-fn prob1() -> u32 {
+pub fn p1() -> u32 {
     // OBJECTIVE: Find the sum of all the multiples of 3 or 5 below 1000.
     // SOLUTION:
     // The sum of all numbers divisible by 3 is:
-    // n=1 to 333: Σ(3n)
-    // --> n=1 to 333: 3*Σ(n)
-    // --> 3*(333*334)/2
+    // n=1 to 333: Σ(3n) --> n=1 to 333: 3*Σ(n) --> 3*(333*334)/2
     let threes: u32 = 3 * 333 * 344 / 2;
     // The sum of all numbers divisible by 5 is:
-    // n=1 to 199: Σ(5n)
-    // --> n=1 to 199: 5*Σ(n)
-    // --> 5*(199*200)/2
+    // n=1 to 199: Σ(5n) --> n=1 to 199: 5*Σ(n) --> 5*(199*200)/2
     let fives: u32 = 5 * 199 * 200 / 2;
     // We count multiples of 15 in both the sums above we need to subtract them out
     // The sum of all numbers divisible by 15 is:
-    // n=1 to 66: Σ(15n)
-    // --> n=1 to 66: Σ(15n)
-    // --> 15*(66*67)/2
+    // n=1 to 66: Σ(15n) --> n=1 to 66: Σ(15n) --> 15*(66*67)/2
     let fifteens: u32 = 15 * 66 * 67 / 2;
     // Finally add threes and fives and subtract fifteens to yield the answer!
     threes + fives - fifteens
