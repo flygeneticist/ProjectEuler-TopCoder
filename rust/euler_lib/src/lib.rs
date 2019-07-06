78b9e4a412c9aaae6e4da35d5999cc86e9013189
Kevin Keller <flygeneticist@gmail.com>
2019-07-06 15:17:30 +0700

    completed problem #12

--- rust/euler_lib/src/lib.rs
+++ rust/euler_lib/src/lib.rs
@@ -1,3 +1,18 @@
-pub fn triangle_number(n: i32) -> () {
+pub fn triangle_number(n: i32) -> i32 {
+    // find the sum of natural numbers where n represents the Nth Triangle number
+    (n*(n+1))/2
+}
 
+pub fn find_factors(n: i32) -> Vec<i32> {
+    // find all factors of a given number
+	let mut factors = vec![];
+    let n_float = n as f32;
+	let x = n_float.sqrt().ceil() as i32;
+    for i in 1..x {
+        if n%i == 0 {
+            factors.push(i);
+            factors.push(n/i);
+        }
+    }
+	factors
 }
